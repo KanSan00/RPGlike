e0942ef3b8589bfaa923237cef61402f42216fa1
KANTA ARAI <akgame36@gmail.com>
2026-05-29 10:29:05 +0900

    処理が肥大化してきていたので、関数に処理分けした

--- src/com/arakan/rpglike/model/Game.java
+++ src/com/arakan/rpglike/model/Game.java
@@ -8,13 +8,13 @@ import com.arakan.rpglike.character.enemy.EnemyFactory;
 import com.arakan.rpglike.character.player.Player;
 
 public class Game {
-	private static final int ACTION_GO = 1;
-	private static final int ACTION_ATTACK = 1;
-	private static final int ACTION_STAND = 2;
-	private static final int ACTION_ESCAPE = 0;
+	
+	private enum MainMenu{GO, QUIT}
+	private enum BattleStartMenu{BATTLE, ESCAPE}
+	private enum BattleMenu{ATTACK, STAND, ESCAPE}
+	
 	Player player;
 	Enemy enemy;
-	Boolean isEscape = false;
 	
 	Scanner scanner;
 	Random rand;
@@ -32,113 +32,141 @@ public class Game {
 		String name = this.scanner.nextLine();
 		this.player.setName(name);
 		
-		while(this.player.getHp() > 0) {			
-			int action = selectAction();			
-			if(action == ACTION_GO) {
-				int num = this.rand.nextInt(2);
-				if(num == 0) {
-					System.out.println("特に何もない先に進もう");
-					continue;
-				}
-				else if(num == 1) {	
-					System.out.println("敵が現れた！");
-					battle();
-				}
-			}else {
+		while (this.player.getHp() > 0) {			
+			MainMenu command = selectMainMenu();			
+			if (command == MainMenu.GO) {
+				handleExploration();
+			} else {
 				System.out.println("冒険をやめた");
 				break;
 			}
 		}
 	}
 	
-	public void battle() {
-		int action = -1;
-		createEnemy();
-		isEscape = false;
-		while(!isEscape) {
-			System.out.println(this.player.getName()+" Lv."+player.getLevel()+ "\nHP: "+this.player.getHp());
-			System.out.println(this.enemy.getName() + "\nHP: "+this.enemy.getHp());
-			action = selectBattleAction();
-			if(action == ACTION_ATTACK) {
-				player.attack(enemy);
-				
-				if(enemy.isDead()) {
-					System.out.println(enemy.getName()+"を倒した！");
-					player.gainExp(enemy.getExp());
-					break;
-				}
-				enemy.attack(player);
-			}
-			else if(action == ACTION_STAND) {
-				int num = this.rand.nextInt(2);
-				if(num == 0) {					
-					enemy.attack(player);
-				}else if(num == 1) {
-					System.out.println(enemy.getName()+"は様子を見ている！");
-					continue;
-				}
-			}
-			else if(action == ACTION_ESCAPE) {
-				int num = this.rand.nextInt(2);
-				if(num == 0) {	
-					System.out.println("逃げられなかった...");
-					enemy.attack(player);
-					isEscape = false;
-				}else if(num == 1) {
-					System.out.println("逃げた...");
-					isEscape = true;
-					break;
-				}
+	// 進む処理を独立させ、start()の見通しを良くする
+	private void handleExploration() {
+		// rand.nextBoolean() で 50% の確率をシンプルに表現
+		if (this.rand.nextBoolean()) {
+			System.out.println("特に何もない先に進もう");
+		} else {	
+			System.out.println("敵が現れた！");
+			if (selectBattleStartMenu() == BattleStartMenu.BATTLE) {
+				battle();						
+			} else {
+				System.out.println("逃げた...");
 			}
 		}
-		
 	}
 	
+	private void battle() {
+		// エネミーの生成
+		createEnemy();
+		boolean isBattleOver = false;
+		
+		// ループの条件を「バトルが終了していない間」に変更
+		while (!isBattleOver) {
+			printStatus();
+			BattleMenu command = selectBattleMenu();
+			
+			// switch文を使い、コマンドごとの処理をメソッドに丸投げする
+			switch (command) {
+				case ATTACK:
+					isBattleOver = executeAttackAction();
+					break;
+				case STAND:
+					executeStandAction();
+					break;
+				case ESCAPE:
+					isBattleOver = executeEscapeAction();
+					break;
+			}
+		}
+	}
+	
+	// 3. バトル中の各アクションをメソッドとして抽出（単一責任）
+	
+	private void printStatus() {
+		System.out.println(player.getName() + " Lv." + player.getLevel() + "　HP: " + player.getHp());
+		System.out.println(enemy.getName() + " HP: " + enemy.getHp());
+	}
+
+	private boolean executeAttackAction() {
+		player.attack(enemy);
+		if (enemy.isDead()) {
+			System.out.println(enemy.getName() + "を倒した！");
+			player.gainExp(enemy.getExp());
+			return true; // バトル終了
+		}
+		enemy.attack(player);
+		return player.isDead(); // プレイヤーが死んだらバトル終了
+	}
+
+	private void executeStandAction() {
+		if (this.rand.nextBoolean()) {					
+			enemy.attack(player);
+		} else {
+			System.out.println(enemy.getName() + "は様子を見ている！");
+		}
+	}
+
+	private boolean executeEscapeAction() {
+		if (this.rand.nextBoolean()) {	
+			System.out.println("逃げた...");
+			return true; // バトル終了
+		} else {
+			System.out.println("逃げられなかった...");
+			enemy.attack(player);
+			return player.isDead();
+		}
+	}
+	
+	/**
+	 * ランダムにエネミーを生成
+	 */
 	public void createEnemy() {
 		this.enemy = enemyFactory.createRandomEnemy();
 	}
 	
-	private int selectAction() {
-		String startLog =  String.format(
-	            "[%d] 先に進む, [%d] 冒険をやめる",
-	            ACTION_GO,
-	            ACTION_ESCAPE
-	        );
-	        System.out.println(startLog);
-	        
-	        int action = -1;
-	        while(action != ACTION_GO && action != ACTION_ESCAPE) {
-	            String input = this.scanner.nextLine();
-	            try {
-	                action = Integer.parseInt(input);
-	            }
-	            catch(Exception e) {
-	            }
-	        }
-	        return action;
-	}
+	// 2. 入力処理の共通化ヘルパーメソッド
+		private int promptForInput(String message, int maxChoice) {
+			System.out.println(message);
+			while (true) {
+				String input = this.scanner.nextLine();
+				try {
+					int action = Integer.parseInt(input);
+					if (action >= 0 && action <= maxChoice) {
+						return action;
+					}
+				} catch (NumberFormatException e) {
+					// 数字以外が入力された場合は無視してループを続ける
+				}
+				System.out.println("正しい番号を入力してください。");
+			}
+		}
 	
-	private int selectBattleAction(){
-
-		String commandLog =  String.format(
-	            "[%d] 攻撃, [%d] 様子を見る, [%d] 逃げる",
-	            ACTION_ATTACK,
-	            ACTION_STAND,
-	            ACTION_ESCAPE
-	        );
-	        System.out.println(commandLog);
-	        
-	        int action = -1;
-	        while(action != ACTION_ATTACK 
-	        		&& action != ACTION_STAND
-	        		&& action != ACTION_ESCAPE) {
-	            String input = this.scanner.nextLine();
-	            try {
-	                action = Integer.parseInt(input);
-	            }
-	            catch(Exception e) {
-	            }
-	        }
-	        return action;
-	}
+	
+	
+		private MainMenu selectMainMenu() {
+			String msg = String.format("[%d] 先に進む, [%d] 冒険をやめる", 1, 0);
+			int choice = promptForInput(msg, 1);
+			// １ならGO、0ならQUIT
+			return choice == 1 ? MainMenu.GO : MainMenu.QUIT;
+		}
+		
+		private BattleStartMenu selectBattleStartMenu() {
+			String msg = String.format("[%d] 戦う, [%d] 逃げる", 1, 0);
+			int choice = promptForInput(msg, 1);
+			return choice == 1 ? BattleStartMenu.BATTLE : BattleStartMenu.ESCAPE;
+		}
+		
+		private BattleMenu selectBattleMenu() {
+			String msg = String.format("[%d] 攻撃, [%d] 様子を見る, [%d] 逃げる", 1, 2, 0);
+			int choice = promptForInput(msg, 2);
+			
+			switch (choice) {
+				case 1:  return BattleMenu.ATTACK;
+				case 2:  return BattleMenu.STAND;
+				default: return BattleMenu.ESCAPE;
+			}
+		}
 }
