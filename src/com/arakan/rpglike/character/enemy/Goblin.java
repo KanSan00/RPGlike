a5bbdd1ffe1d3208ff30a4ce3a4b54aa573a4d64
KANTA ARAI <akgame36@gmail.com>
2026-05-29 12:42:59 +0900

    GoblinおよびSlimeクラスのコメントを修正し、最大HPの説明を追加

--- src/com/arakan/rpglike/character/enemy/Goblin.java
+++ src/com/arakan/rpglike/character/enemy/Goblin.java
@@ -2,6 +2,7 @@ package com.arakan.rpglike.character.enemy;
 
 public class Goblin extends Enemy {
     public Goblin() {
+    	// 名前, HP, 最大HP, 防御力, 攻撃力, 経験値 を親クラスに渡す
         super("ゴブリン", 30, 20, 1, 5, 250);
     }
 }
--- src/com/arakan/rpglike/character/enemy/Slime.java
+++ src/com/arakan/rpglike/character/enemy/Slime.java
@@ -3,7 +3,7 @@ package com.arakan.rpglike.character.enemy;
 public class Slime extends Enemy {
 
 	public Slime() {
-		// 名前, HP, 防御力, 攻撃力, 経験値 を親クラスに渡す
+		// 名前, HP, 最大HP, 防御力, 攻撃力, 経験値 を親クラスに渡す
         super("スライム", 10, 10, 1, 2, 100);
 	}
 
